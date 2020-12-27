b9793927ecade637d5410e15fe8b354cfe771e11
Vipin Kumar <vipin.ind.kumar@gmail.com>
2020-12-27 12:13:22 +0530

    for slight boost in performnce remove redundent call for lookup in dictionary

---
+++ 2sum/2sum2.py
@@ -0,0 +1,14 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # returns index of two integers in nums that add to target
+    # create a hash map of nums, for faster lookup
+    numsHash = {nums[i]: i for i in range(len(nums))}
+    
+    for i, firstInt in enumerate(nums):
+        lkup = target - firstInt
+        
+        if lkup in numsHash:
+            lkupIndex = numsHash[lkup]
+            if i != lkupIndex:
+                return [i, lkupIndex]
+    
+    return "not working"
